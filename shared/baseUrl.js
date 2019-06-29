608b8bd35040cc23aff2a287a8f1022afba75377
owen <owendavies802@gmail.com>
2019-06-29 10:27:36 -0400

    Sending Email

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://10.1.10.228: 3001/';
+//export const baseUrl = 'http://10.1.10.228: 3001/';
 
 //export const baseUrl = 'http://192.168.1.215:3001/';
-//export const baseUrl = 'http://10.0.0.102:3001/';
+export const baseUrl = 'http://10.0.0.102:3001/';
